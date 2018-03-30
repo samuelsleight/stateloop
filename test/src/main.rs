90121a2a72161acee859d75df37c9f211c314749
Samuel Sleight <samuel.sleight@gmail.com>
2018-03-30 21:53:47 +0100

    Working vulkan test!

--- test/src/main.rs
+++ test/src/main.rs
@@ -6,8 +6,9 @@
 extern crate vulkano_win;
 
 use std::{ptr, mem};
+use std::rc::Rc;
 use std::sync::Arc;
-use std::cell::{Cell, UnsafeCell};
+use std::cell::{Cell, RefCell, UnsafeCell};
 
 use stateloop::app::{App, Data, Event, Window, WindowBuilder};
 use stateloop::state::Action;
@@ -21,7 +22,7 @@ use vulkano::image::SwapchainImage;
 use vulkano::pipeline::{GraphicsPipeline, GraphicsPipelineAbstract};
 use vulkano::pipeline::viewport::Viewport;
 use vulkano::swapchain::{self, PresentMode, SurfaceTransform, Surface, Swapchain,AcquireError, SwapchainCreationError};
-use vulkano::sync::{now, GpuFuture};
+use vulkano::sync::{now, GpuFuture, FlushError};
 
 use vulkano_win::VkSurfaceBuild;
 
@@ -40,6 +41,10 @@ states! {
 }
 
 struct Renderer {
+    data: RefCell<RendererData>
+}
+
+struct RendererData {
     device: Arc<Device>,
     queue: Arc<Queue>,
     swapchain: Arc<Swapchain<Window>>,
@@ -50,8 +55,9 @@ struct Renderer {
     render_pass: Arc<RenderPassAbstract + Send + Sync>,
     framebuffers: Option<Vec<Arc<FramebufferAbstract + Sync + Send>>>,
 
+    dimensions: [u32; 2],
     frame_future: UnsafeCell<Box<GpuFuture>>,
-    recreate_swapchain: Cell<bool>
+    recreate_swapchain: bool
 }
 
 impl MainHandler for Data<Renderer, Arc<Surface<Window>>> {
@@ -62,43 +68,10 @@ impl MainHandler for Data<Renderer, Arc<Surface<Window>>> {
         }
     }
 
-    fn handle_tick(&mut self) {
-        let (w, h) = self.window().window().get_inner_size().unwrap();
-        let renderer = &mut self.data;
-
-        if renderer.recreate_swapchain.get() {
-
-            let (new_swapchain, new_images) = match renderer.swapchain.recreate_with_dimension([w, h]) {
-                Ok(r) => r,
-                Err(SwapchainCreationError::UnsupportedDimensions) => return,
-                Err(err) => panic!("{:?}", err)
-            };
-
-            mem::replace(&mut renderer.swapchain, new_swapchain);
-            mem::replace(&mut renderer.images, new_images);
-
-            renderer.framebuffers = None;
-            renderer.recreate_swapchain.set(false);
-        }
-
-        if renderer.framebuffers.is_none() {
-            let new_framebuffers = Some(renderer.images.iter().map(|image| {
-                Arc::new(Framebuffer::start(renderer.render_pass.clone())
-                     .add(image.clone())
-                     .unwrap()
-                     .build()
-                     .unwrap())
-            })
-                .map(|fb| fb as Arc<FramebufferAbstract + Sync + Send>)
-                .collect::<Vec<_>>());
-
-            mem::replace(&mut renderer.framebuffers, new_framebuffers);
-        }
-    }
+    fn handle_tick(&mut self) {}
 
     fn handle_render(&self) {
-        let (w, h) = self.window().window().get_inner_size().unwrap();
-        let renderer = &self.data;
+        let mut renderer = self.data.data.borrow_mut();
 
         let mut frame_future = unsafe { 
             let ptr = renderer.frame_future.get();
@@ -107,61 +80,106 @@ impl MainHandler for Data<Renderer, Arc<Surface<Window>>> {
 
         frame_future.cleanup_finished();
 
-        let (image_num, acquire_future) = match swapchain::acquire_next_image(renderer.swapchain.clone(), None) {
-            Ok(r) => r,
-            Err(AcquireError::OutOfDate) => {
-                renderer.recreate_swapchain.set(true);
-                return
-            },
-            Err(err) => panic!("{:?}", err)
-        };
+        loop {
+            if renderer.recreate_swapchain {
+                renderer.dimensions = self.window().capabilities(renderer.device.physical_device())
+                    .expect("failed to get surface capabilities")
+                    .current_extent
+                    .unwrap();
 
-        let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(renderer.device.clone(), renderer.queue.family())
-            .unwrap()
-            .begin_render_pass(
-                renderer.framebuffers.as_ref().unwrap()[image_num].clone(),
-                false,
-                vec![[1.0, 0.0, 1.0, 1.0].into()]
-            )
-            .unwrap()
-            .draw(
-                renderer.pipeline.clone(),
-                DynamicState {
-                    line_width: None,
-                    viewports: Some(vec![Viewport {
-                          origin: [0.0, 0.0],
-                          dimensions: [w as f32, h as f32],
-                          depth_range: 0.0 .. 1.0,
-                    }]),
-                    scissors: None
+                let (new_swapchain, new_images) = match renderer.swapchain.recreate_with_dimension(renderer.dimensions) {
+                    Ok(r) => r,
+                    Err(SwapchainCreationError::UnsupportedDimensions) => continue,
+                    Err(err) => panic!("{:?}", err)
+                };
+
+                mem::replace(&mut renderer.swapchain, new_swapchain);
+                mem::replace(&mut renderer.images, new_images);
+
+                renderer.framebuffers = None;
+                renderer.recreate_swapchain = false;
+            }
+
+            if renderer.framebuffers.is_none() {
+                let new_framebuffers = Some(renderer.images.iter().map(|image| {
+                    Arc::new(Framebuffer::start(renderer.render_pass.clone())
+                        .add(image.clone())
+                        .unwrap()
+                        .build()
+                        .unwrap())
+                })
+                    .map(|fb| fb as Arc<FramebufferAbstract + Sync + Send>)
+                    .collect::<Vec<_>>());
+
+                mem::replace(&mut renderer.framebuffers, new_framebuffers);
+            }
+
+            let (image_num, acquire_future) = match swapchain::acquire_next_image(renderer.swapchain.clone(), None) {
+                Ok(r) => r,
+                Err(AcquireError::OutOfDate) => {
+                    renderer.recreate_swapchain = true;
+                    continue
                 },
-                vec![renderer.vertex_buffer.clone()],
-                (), ()
-            )
-            .unwrap()
-            .end_render_pass()
-            .unwrap()
-            .build()
-            .unwrap();
+                Err(err) => panic!("{:?}", err)
+            };
 
-        let future = frame_future
-            .join(acquire_future)
-            .then_execute(
-                renderer.queue.clone(), 
-                command_buffer
-            )
-            .unwrap()
-            .then_swapchain_present(
-                renderer.queue.clone(), 
-                renderer.swapchain.clone(), 
-                image_num
-            )
-            .then_signal_fence_and_flush()
-            .unwrap();
+            let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(renderer.device.clone(), renderer.queue.family())
+                .unwrap()
+                .begin_render_pass(
+                    renderer.framebuffers.as_ref().unwrap()[image_num].clone(),
+                    false,
+                    vec![[1.0, 0.0, 1.0, 1.0].into()]
+                )
+                .unwrap()
+                .draw(
+                    renderer.pipeline.clone(),
+                    DynamicState {
+                        line_width: None,
+                        viewports: Some(vec![Viewport {
+                              origin: [0.0, 0.0],
+                              dimensions: [renderer.dimensions[0] as f32, renderer.dimensions[1] as f32],
+                              depth_range: 0.0 .. 1.0,
+                        }]),
+                        scissors: None
+                    },
+                    vec![renderer.vertex_buffer.clone()],
+                    (), ()
+                )
+                .unwrap()
+                .end_render_pass()
+                .unwrap()
+                .build()
+                .unwrap();
 
-        unsafe {
-            let ptr = renderer.frame_future.get();
-            ptr::write(ptr, Box::new(future) as Box<_>);
+            let future = frame_future
+                .join(acquire_future)
+                .then_execute(
+                    renderer.queue.clone(),
+                    command_buffer
+                )
+                .unwrap()
+                .then_swapchain_present(
+                    renderer.queue.clone(),
+                    renderer.swapchain.clone(),
+                    image_num
+                )
+                .then_signal_fence_and_flush();
+
+            let end_future = match future {
+                Ok(future) => Box::new(future) as Box<_>,
+                Err(FlushError::OutOfDate) => {
+                    renderer.recreate_swapchain = true;
+                    Box::new(now(renderer.device.clone())) as Box<_>
+                },
+                Err(e) => Box::new(now(renderer.device.clone())) as Box<_>
+            };
+
+            unsafe {
+                let ptr = renderer.frame_future.get();
+                ptr::write(ptr, end_future);
+            }
+
+            break
         }
     }
 }
@@ -173,11 +191,14 @@ impl TestHandler for Data<Renderer, Arc<Surface<Window>>> {
 
     fn handle_tick(&mut self, _: usize) {}
 
-    fn handle_render(&self, _: usize) {
-    }
+    fn handle_render(&self, _: usize) {}
 }
 
 fn init_vulkan(instance: Arc<Instance>, window: &Arc<Surface<Window>>) -> Renderer {
+    for device in PhysicalDevice::enumerate(&instance) {
+        println!("Device: {} (type: {:?})", device.name(), device.ty());
+    }
+
     // Select physical device
     let physical = PhysicalDevice::enumerate(&instance)
         .next()
@@ -209,7 +230,7 @@ fn init_vulkan(instance: Arc<Instance>, window: &Arc<Surface<Window>>) -> Render
 
     let queue = queues.next().unwrap();
 
-    let (w, h) = window.window().get_inner_size().unwrap();
+    let mut dimensions;
 
     // Create swapchain
     let (swapchain, images) = {
@@ -219,12 +240,14 @@ fn init_vulkan(instance: Arc<Instance>, window: &Arc<Surface<Window>>) -> Render
         let alpha = caps.supported_composite_alpha.iter().next().unwrap();
         let format = caps.supported_formats[0].0;
 
+        dimensions = caps.current_extent.unwrap_or([1024, 768]);
+
         Swapchain::new(
             device.clone(), 
             window.clone(),
             caps.min_image_count,
             format,
-            [w, h],
+            dimensions,
             1,
             caps.supported_usage_flags,
             &queue,
@@ -315,18 +338,21 @@ void main() {
     */
 
     Renderer {
-        device: device.clone(),
-        queue: queue,
-        swapchain: swapchain,
-        images: images,
+        data: RefCell::new(RendererData {
+            device: device.clone(),
+            queue,
+            swapchain,
+            images,
 
-        vertex_buffer: vertex_buffer,
-        pipeline: pipeline as Arc<GraphicsPipelineAbstract + Send + Sync>,
-        render_pass: render_pass as Arc<RenderPassAbstract + Send + Sync>,
-        framebuffers: None,
+            vertex_buffer,
+            pipeline: pipeline as Arc<GraphicsPipelineAbstract + Send + Sync>,
+            render_pass: render_pass as Arc<RenderPassAbstract + Send + Sync>,
+            framebuffers: None,
 
-        frame_future: UnsafeCell::new(Box::new(now(device.clone())) as Box<GpuFuture>),
-        recreate_swapchain: Cell::new(false)
+            dimensions,
+            frame_future: UnsafeCell::new(Box::new(now(device.clone())) as Box<GpuFuture>),
+            recreate_swapchain: false
+        })
     }
 }
 
